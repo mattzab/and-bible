a944ed478dee433ab67c5d67becf878725645813
mattzab <mattzab@gmail.com>
2017-01-12 10:11:07 -0700

    Update RepoFactory.java

--- and-bible/app/src/main/java/net/bible/service/download/RepoFactory.java
+++ and-bible/app/src/main/java/net/bible/service/download/RepoFactory.java
@@ -4,7 +4,7 @@ import org.crosswire.jsword.book.Book;
 
 /**
  * @author Martin Denham [mjdenham at gmail dot com]
- * @see gnu.lgpl.License for license details.<br>
+ * @see gnu.lgpl.License for license details.<br>asdf
  *      The copyright to this program is held by it's author.
  */
 public class RepoFactory {
